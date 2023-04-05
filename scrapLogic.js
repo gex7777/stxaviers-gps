ab854f0b9b25b23ad67c3240fd0a91409c1c009f
George <gex7777@gmail.com>
2023-04-06 03:24:44 +0530

    fix12

--- scrapLogic.js
+++ scrapLogic.js
@@ -37,9 +37,6 @@ const scrapLogic = async (res, myCache) => {
         }, 1000);
       };
     });
-    await page.setUserAgent(
-      "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36"
-    );
 
     // Set screen size
 
